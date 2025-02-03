6e27ac8723e009fc9922f174a7b87f9c58816ec2
doncici77 <pkh2956@naver.com>
2025-02-03 16:52:13 +0900

    매개변수, 오버로딩

--- L20250203/Program.cs
+++ L20250203/Program.cs
@@ -4,6 +4,34 @@ namespace L20250203
 {
     internal class Program
     {
+        //반환형 함수명(자료형 인자1, 자료형 인자2, ...)
+        //{
+        //  return 자료반환;
+        //}
+
+        static float Multiply(float number, float number2)
+        {
+            return number * number2;
+        }
+
+        static int Multiply(int number, int number2)
+        {
+            return number * number2;
+        }
+
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int a = 0; // 변수 선언할때 초기화 무조건 해주는 것이 좋음 
+            int b = 0; // 변수 선언할때 ',' 쓰지말고 따로따로 선언하는 것이 좋음
+
+            a = int.Parse(input[0]);
+            b = int.Parse(input[1]);
+
+            Console.WriteLine(Multiply(2.3f, 3.5f));
+        }
+
+        /*// 함수, 지역 변수, 전역 변수
         static int[,] data = new int[10, 10];
 
         static void Initialze() //PascalCase
@@ -39,7 +67,13 @@ namespace L20250203
             Initialze();
             Print();
 
-            /*for (int i = 1; i <= size; i++)
+        }*/
+
+        /*// 2중 반복문
+        static void Main(string[] args)
+        {
+
+            for (int i = 1; i <= size; i++)
             {
                 for (int j = 0; j < (size - i); j++)
                 {
@@ -64,7 +98,7 @@ namespace L20250203
                     Console.Write('*');
                 }
                 Console.WriteLine();
-            }*/
-        }
+            }
+        }*/
     }
 }
