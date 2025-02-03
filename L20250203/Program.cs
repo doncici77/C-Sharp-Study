38196fc0af24656c664c20a199e0ba4a4c6156b0
doncici77 <pkh2956@naver.com>
2025-02-03 15:12:34 +0900

    2차원 배열에 부터 100까지 넣어보기

--- L20250203/Program.cs
+++ L20250203/Program.cs
@@ -4,16 +4,48 @@
     {
         static void Main(string[] args)
         {
-            int size = 20;
-            int[,] data = new int[1080, 1920];
+            //[][][][][][][][][][]
+            //[][][][][][][][][][]
+            //[][][][][][][][][][]
+            //[][][][][][][][][][]
+            //[][][][][][][][][][]
+            //[][][][][][][][][][]
+            //[][][][][][][][][][]
+            //[][][][][][][][][][]
+            //[][][][][][][][][][]
+            //[][][][][][][][][][]
 
-            for(int i = 1; i <= size; i++)
+            int size = 10;
+            int[ , ] data = new int[10, 10];
+            string s = "Hello world!";
+
+            //초기화
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    data[i , j] = 1 + j + i * size; // 들어가는 데이터 값 계산 후 초기화
+                }
+            }
+
+            //출력
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write($"data[{i.ToString()}, {j.ToString()}] = {data[i, j].ToString()}  "); // 데이어터 생성 확인 내용 출력
+                }
+                Console.WriteLine();
+            }
+
+
+            /*for (int i = 1; i <= size; i++)
             {
                 for (int j = 0; j < (size - i); j++)
                 {
                     Console.Write(" ");
                 }
-                
+
                 for (int k = 0; k < i; k++)
                 {
                     Console.Write('*');
@@ -32,7 +64,7 @@
                     Console.Write('*');
                 }
                 Console.WriteLine();
-            }
+            }*/
         }
     }
 }
