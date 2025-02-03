ada56d8756b9ae7f8446b3dc1d78c60e4db56abf
doncici77 <pkh2956@naver.com>
2025-02-03 15:53:29 +0900

    함수,변수 범위

--- L20250203/Program.cs
+++ L20250203/Program.cs
@@ -1,43 +1,43 @@
-﻿namespace L20250203
+﻿using System.Drawing;
+
+namespace L20250203
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            //[][][][][][][][][][]
-            //[][][][][][][][][][]
-            //[][][][][][][][][][]
-            //[][][][][][][][][][]
-            //[][][][][][][][][][]
-            //[][][][][][][][][][]
-            //[][][][][][][][][][]
-            //[][][][][][][][][][]
-            //[][][][][][][][][][]
-            //[][][][][][][][][][]
+        static int[,] data = new int[10, 10];
 
-            int size = 10;
-            int[ , ] data = new int[10, 10];
-            string s = "Hello world!";
+        static void Initialze() //PascalCase
+        {
+            int num = 1;
 
             //초기화
-            for (int i = 0; i < size; i++)
+            for (int i = 0; i < 10; i++)
             {
-                for (int j = 0; j < size; j++)
+                for (int j = 0; j < 10; j++)
                 {
-                    data[i , j] = 1 + j + i * size; // 들어가는 데이터 값 계산 후 초기화
+                    data[i, j] = num; // 들어가는 데이터 값 계산 후 초기화
+                    num++;
                 }
             }
+        }
 
+        static void Print()
+        {
             //출력
-            for (int i = 0; i < size; i++)
+            for (int i = 0; i < 10; i++)
             {
-                for (int j = 0; j < size; j++)
+                for (int j = 0; j < 10; j++)
                 {
                     Console.Write($"data[{i.ToString()}, {j.ToString()}] = {data[i, j].ToString()}  "); // 데이어터 생성 확인 내용 출력
                 }
                 Console.WriteLine();
             }
+        }
+        static void Main(string[] args)
+        {
 
+            Initialze();
+            Print();
 
             /*for (int i = 1; i <= size; i++)
             {
