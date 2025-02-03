0b2fa2e4a65f6a3aac8e38213253fd5345eefaed
doncici77 <pkh2956@naver.com>
2025-02-03 14:24:52 +0900

    이중 for문 연습

--- L20250203/Program.cs
+++ L20250203/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int size = 20;
+            int[,] data = new int[1080, 1920];
+
+            for(int i = 1; i <= size; i++)
+            {
+                for (int j = 0; j < (size - i); j++)
+                {
+                    Console.Write(" ");
+                }
+                
+                for (int k = 0; k < i; k++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+            for (int i = size; i > 1; i--)
+            {
+                for (int j = 0; j < (size - i); j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = 0; k < i; k++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
