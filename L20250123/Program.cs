12f2578a41d49e929df7998a682d6e50570f7d7b
doncici77 <pkh2956@naver.com>
2025-02-11 16:23:56 +0900

    1,2일차 c#수업

---
+++ L20250123/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace L20250123
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int -> 정수 : 10, 20, 30, 1, 2, -1
+            //float -> 실수 : 0.1, 0.0032, 1.54
+            //char -> 유니코드 한글자 'A' -> 65
+            // +, -, *, /, %
+
+            //int koreanScore = 10; //camelcase 낙타 등처럼 생겨서, 대문자로 띄어쓰기를 의미.
+            //int korean_score; //snakecase 뱀 처럼 길어진다해서, _(언더바)를 이용한 변수 작성법.
+            //float average = (float)sum / 3.0f; //f를 넣는 이유는 float라도 인식하게 하기 위함
+            //average = 0; //0.0f와 일반적인 0은 차이가 없다.
+
+        }
+    }
+}
+#region 필기내용
+//OOP 객체 지향 프로그래밍
+//F9 중단점 //점 찍힌 부분까지만 실행
+//Ctrl + K + D 누르면 코드 줄정리가 자동으로 됨
+//Ctrl + y 앞으로 가기 //Ctrl + z 뒤로가기
+//데이터 타입에 byte도 있음 숫자만 됨
+#endregion
---
+++ L20250124/Program.cs
@@ -0,0 +1,109 @@
+﻿using System.Numerics;
+
+namespace L20250124
+{
+    class Color
+    {
+        public int R;
+        public int G;
+        public int B;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char wall = '*';
+            char floor = ' ';
+            int playerX = 1;
+            int playerY = 1;
+
+
+            //class, OOP
+            Color[,] picture = new Color[240, 640];
+
+
+            int[,] map =
+                {
+                    { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                    { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
+                };
+
+            while (true)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+                Console.Clear();
+
+                //process update
+                if (keyInfo.Key == ConsoleKey.W)
+                {
+                    playerY--;
+                }
+                else if (keyInfo.Key == ConsoleKey.S)
+                {
+                    playerY++;
+                }
+                else if (keyInfo.Key == ConsoleKey.A)
+                {
+                    playerX--;
+                }
+                else if (keyInfo.Key == ConsoleKey.D)
+                {
+                    playerX++;
+                }
+
+                //Draw Map -> Render
+                for (int y = 0; y < 10; y++)
+                {
+                    for (int x = 0; x < 10; x++)
+                    {
+                        if (x == playerX && y == playerY)
+                        {
+                            Console.Write('P');
+                        }
+                        else if (x == 7 && y == 7)
+                        {
+                            Console.Write('M');
+                        }
+                        else if (map[y, x] == 1)
+                        {
+                            Console.Write(wall);
+                        }
+                        else if (map[y, x] == 0)
+                        {
+                            Console.Write(floor);
+                        }
+                    }
+                    Console.Write("\n");
+                }
+            }
+        }
+    }
+}
+
+#region 2025-01-24 필기내용
+// int // -2^31 ~ 0 ~ 2^31-1 // 4byte = 8비트 * 4 = 32비트
+// unsigned int // -부호가 없어져서 크기가 2^32-1 까지 가능
+// byte = 1바이트, short = 2바이트, int = 4바이트, long = 8바이트
+//"" 이전에 $를 넣으면 문자열에서 {}괄호안에 변수를 삽입가능
+
+//논리연산자 논리합: AND -> &&, 논리곱: OR -> ||, NOT -> !
+
+//array 배열
+// 자료형[] 변수명 = new 자료형[개수]
+
+//for문
+//for(변수 선언 혹은 초기화; 조건문; 증감문)
+//{
+//}
+//배열 탐색을 위한 문법
+
+#endregion
