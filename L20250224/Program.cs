3d7ab601901de2b58b1c8c451b02767e4c5a34ad
doncici77 <pkh2956@naver.com>
2025-02-24 11:38:02 +0900

    비트 연산자 예제문제

--- L20250224/Program.cs
+++ L20250224/Program.cs
@@ -2,8 +2,49 @@
 {
     internal class Program
     {
+        class BitArray32
+        {
+            public uint Data;
+
+            public void On(int position)
+            {
+                if(position > 0 && position <= 32)
+                {
+                    Data = Data | (uint)(1 << (position - 1));
+                }
+            }
+
+            public void Off(int position)
+            {
+                if (position > 0 && position <= 32)
+                {
+                    Data = Data & ~(uint)(1 << (position - 1));
+                }
+            }
+
+            public bool Check(uint other)
+            {
+                 return (int)(Data & other) > 0 ? true : false;
+            }
+        }
+
         static void Main(string[] args)
         {
+            BitArray32 bitArray = new BitArray32();
+            BitArray32 bitArray2 = new BitArray32();
+            bitArray.On(3); // 비트 자리수
+            bitArray.On(1);
+            //0101 = 5
+            Console.WriteLine(bitArray.Data);
+            Console.WriteLine(Convert.ToString(bitArray.Data, 2));
+
+            bitArray.Off(3);
+            Console.WriteLine(bitArray.Data);
+            Console.WriteLine(Convert.ToString(bitArray.Data, 2));
+
+            bitArray.Check(bitArray2.Data);
+            Console.WriteLine(bitArray.Check(bitArray2.Data).ToString());
+
             // << shift 연산자
             // >>
 
@@ -28,6 +69,20 @@
             // 255
 
             // 1111
+
+            byte Player = 1; // => 0b0000 0001
+            byte Camera = 2; // => 0b0000 0010
+            byte UI =     4; // => 0b0000 0100
+            byte Water =  8; // => 0b0000 1000
+
+            byte layer = 0x00000000;
+            layer = (byte)(layer | Player);
+
+            // bit masking
+            if((layer & (Camera | Player)) > 0)
+            {
+                    
+            }
 
             int R = 255;
             R = 0xFF;
---
+++ L20250224_02/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace L20250224_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*// 1번
+            int n = 5;
+            int[] arr1 = arr1 = [9, 20, 28, 18, 11]; 
+            int[] arr2 = arr2 = [30, 1, 21, 17, 28];
+            int[] valueArr = new int[n];
+
+            for (int i = 0; i < valueArr.Length; i++)
+            {
+                valueArr[i] = arr1[i] | arr2[i];
+
+                string value_bit = Convert.ToString(valueArr[i], 2); // 2진수 문자열을 한글자씩 나눔
+                // Console.WriteLine(Convert.ToString(valueArr[i], 2).Replace('1', '#').Replace('0', ' '));
+
+                for(int j = 0; j < value_bit.Length; j++)
+                {
+                    if (value_bit[j] == '0')
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write("*");
+                    }
+                }
+                Console.WriteLine();
+            }   */
+
+
+            // 2번 
+            int n = 3;
+            long[] x = new long[n];
+            long[] npot =  new long[n];
+            long total = 0;
+            x = [3, 5, 7];
+
+            for(int i = 0; i < n; i++)
+            {
+                npot[i] = 0;
+                while (x[i] > 0)
+                {
+                    x[i] = x[i] >> 1;
+                    npot[i]++;
+                }
+                npot[i] = 1 << (int)npot[i]; // 0001 << n칸 이동
+            }
+
+            for(int i = 0;i < n; i++)
+            {
+                total = total ^ npot[i];
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
