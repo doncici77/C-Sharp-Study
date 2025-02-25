f2483fb94d54786cbb6715633887ad8b902c72f4
doncici77 <pkh2956@naver.com>
2025-02-25 11:11:52 +0900

    동적 배열 제네릭을 이용해 구현 연습

---
+++ L20250225/Array.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L20250225
+{
+    public class Array<T>
+    {
+        public T[] data = new T[3];
+        int count = 0;
+
+        public void Add(T index)
+        {
+            if (count < data.Length)
+            {
+                data[count] = index;
+            }
+            else if (count >= data.Length)
+            {
+                T[] dataTemp = new T[data.Length + 1];
+                for (int i = 0; i < data.Length; i++)
+                {
+                    dataTemp[i] = data[i];
+                }
+                dataTemp[count] = index;
+                data = dataTemp;
+            }
+            count++;
+        }
+
+        public void RemoveAt(int ordernum)
+        {
+            T[] dataTemp = new T[data.Length - 1];
+
+            for (int i = 0; i < ordernum - 1; i++)
+            {
+                dataTemp[i] = data[i];
+            }
+
+            for (int i = ordernum; i < count; i++)
+            {
+                dataTemp[i - 1] = data[i];
+            }
+            data = dataTemp;
+            count--;
+        }
+
+        public int Count
+        {
+            get { return count; }
+            set { count = value; }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return data[index];
+            }
+            set
+            {
+                if (index < data.Length)
+                {
+                    data[index] = (T)value;
+                }
+            }
+        }
+    }
+}
---
+++ L20250225/Program.cs
@@ -0,0 +1,70 @@
+﻿
+namespace L20250225
+{
+    internal class Program
+    {
+        // 일반적인 개발 -> 프로그램 -> Try, Catch -> 추천
+        // 게임 -> try, catch X -> 빨리 빨리 성능 최대화
+        // OOP -> Error도 객체 -> 예외
+        // Unity engine core -> C++ // contents -> C# -> script
+        static void Main(string[] args)
+        {
+            /*Array<int> test = new Array<int>();
+            test.Add(1);
+            test.Add(2);
+            test.Add(3);
+            test.Add(4);
+            test.Add(5);
+
+            test.RemoveAt(2);
+            for (int i = 0; i < test.data.Length; i++)
+            {
+                Console.WriteLine(test[i]);
+            }
+            Console.WriteLine("개수: " + test.Count);*/
+
+            try
+            {
+                // Engine.Load();
+                // Engine.Run();
+
+
+                Test();
+                // file open
+                // process
+                // exception
+                int number = 5;
+                if(number == 5)
+                {
+                    throw new CustomException("이건 내가만든거");
+                }
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+            catch(Exception e)
+            {
+                StreamWriter sw = new StreamWriter("log.txt");
+                sw.WriteLine(e.Message);
+                sw.WriteLine(e.StackTrace);
+                sw.Flush();
+                sw.Close();
+            }
+            finally
+            {
+                // 파일 닫기
+                // 네트워크 끊기
+                // 메모리 정리
+                // 텍스처 언로딩
+            }
+        }
+
+        static void Test()
+        {
+            int[] a = new int[5];
+            int b = 0;
+        }
+    }
+}
