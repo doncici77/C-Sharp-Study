b6d6a21f1b47886b869fe8ab88cb49107ac39626
doncici77 <pkh2956@naver.com>
2025-03-05 16:43:01 +0900

    Smooth모드 예제

--- L20250305/Assets/Scripts/LerpSample.cs
+++ L20250305/Assets/Scripts/LerpSample.cs
@@ -4,13 +4,15 @@ public class LerpSample : MonoBehaviour
 {
     public Transform A;
     public Transform B;
-    public Material material;
 
     [Range (0, 1)]
     public float T = 0;
 
     float elapsedTime = 0;
     float sign = 1;
+
+    public Color AColor;
+    public Color BColor;
 
     public AnimationCurve curve;
 
@@ -26,6 +28,6 @@ public class LerpSample : MonoBehaviour
         transform.position = Vector3.Lerp(A.position, B.position, curve.Evaluate(elapsedTime));
         transform.rotation = Quaternion.Slerp(A.rotation, B.rotation, curve.Evaluate(elapsedTime));
 
-        material.color = Color.Lerp(Color.red, Color.blue, curve.Evaluate(elapsedTime));
+        GetComponent<MeshRenderer>().material.SetColor("_BaseColor", Color.Lerp(AColor, BColor, curve.Evaluate(elapsedTime)));
     }
 }
--- L20250305/Assets/Scripts/Plane/CameraController.cs
+++ L20250305/Assets/Scripts/Plane/CameraController.cs
@@ -6,6 +6,11 @@ public class CameraController : MonoBehaviour
     public float positionLagTime = 1.0f;
     public float rotationLagTime = 1.0f;
 
+    Vector3 currnetVelocity;
+    Quaternion currentRotation;
+    public float smoothTime = 0.3f;
+    public float angleSmoothTime = 0.3f;
+
     void Start()
     {
         playerPos = GameObject.FindGameObjectWithTag("MyPlane").transform;
@@ -13,8 +18,38 @@ public class CameraController : MonoBehaviour
 
     void LateUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, playerPos.position, Time.deltaTime * positionLagTime);
-        transform.rotation = Quaternion.Lerp(transform.rotation, playerPos.rotation, Time.deltaTime * rotationLagTime);
+        //transform.position = Vector3.Lerp(transform.position, playerPos.position, Time.deltaTime * positionLagTime);
+        transform.position = Vector3.SmoothDamp(transform.position, playerPos.position, ref currnetVelocity, smoothTime);
+        //transform.rotation = Quaternion.Lerp(transform.rotation, playerPos.rotation, Time.deltaTime * rotationLagTime);
+        transform.rotation = CameraController.SmoothDamp(transform.rotation, playerPos.rotation, ref currentRotation, angleSmoothTime);
         Debug.DrawLine(transform.position, Camera.main.transform.position, UnityEngine.Color.red);
+    }
+
+    public static Quaternion SmoothDamp(Quaternion rot, Quaternion target, ref Quaternion deriv, float time)
+    {
+        if (Time.deltaTime < Mathf.Epsilon) return rot;
+        // account for double-cover
+        var Dot = Quaternion.Dot(rot, target);
+        var Multi = Dot > 0f ? 1f : -1f;
+        target.x *= Multi;
+        target.y *= Multi;
+        target.z *= Multi;
+        target.w *= Multi;
+        // smooth damp (nlerp approx)
+        var Result = new Vector4(
+            Mathf.SmoothDamp(rot.x, target.x, ref deriv.x, time),
+            Mathf.SmoothDamp(rot.y, target.y, ref deriv.y, time),
+            Mathf.SmoothDamp(rot.z, target.z, ref deriv.z, time),
+            Mathf.SmoothDamp(rot.w, target.w, ref deriv.w, time)
+        ).normalized;
+
+        // ensure deriv is tangent
+        var derivError = Vector4.Project(new Vector4(deriv.x, deriv.y, deriv.z, deriv.w), Result);
+        deriv.x -= derivError.x;
+        deriv.y -= derivError.y;
+        deriv.z -= derivError.z;
+        deriv.w -= derivError.w;
+
+        return new Quaternion(Result.x, Result.y, Result.z, Result.w);
     }
 }
