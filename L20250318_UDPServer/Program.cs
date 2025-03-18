d7140fcaeeb1ce8b3ed81ede770d1b7a70cfc2b6
doncici77 <pkh2956@naver.com>
2025-03-18 14:32:49 +0900

    Init

---
+++ L20250318_UDPServer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace L20250318_UDPServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Any, 6000);
+
+            serverSocket.Bind(serverEndPoint);
+
+            byte[] buffer = new byte[1024]; // 이것보다 받는 입장에서 버퍼사이즈가 커야한다.
+            EndPoint clientEndPoint = new IPEndPoint();
+            serverSocket.ReceiveFrom(buffer, ref clientEndPoint);
+        }
+    }
+}
